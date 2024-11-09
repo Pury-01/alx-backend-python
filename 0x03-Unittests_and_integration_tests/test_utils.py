7e760a95d44d894733dc94e9430f3bef17df6b4b
Pury-01 <puryokumu@gmail.com>
2024-11-09 09:36:23 +0300

    parameterize a unit test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """
 Test cases for access_nested_map function.
 """
+
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
@@ -9,27 +10,41 @@ from typing import Any, Dict, Tuple
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test cases for access_nested_map function.
-    """
+    """Test cases for access_nested_map function."""
 
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
-        ])
+    ])
     def test_access_nested_map(
             self,
             nested_map: Dict[str, Any],
             path: Tuple[str, ...],
             expected: Any
-            ) -> None:
-        """
-        test access_nested_map function with various inputs.
-        """
+    ) -> None:
+        """Test access_nested_map function with various inputs."""
         self.assertEqual(
-                access_nested_map(nested_map, path),
-                expected
-                )
+            access_nested_map(nested_map, path),
+            expected
+        )
+
+    @parameterized.expand([
+        ({}, ("a",)),            # Empty map, path = ("a",)
+        ({"a": 1}, ("a", "b")),  # Missing key "b" inside "a"
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict[str, Any],
+            path: Tuple[str, ...]
+    ) -> None:
+        """Test that a KeyError is raised for invalid paths
+        """
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+        # Exception message contains the key
+        self.assertIn(path[-1], str(context.exception))
 
 
 if __name__ == '__main__':
